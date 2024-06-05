d7e46b044e3f99d9b032868195b33521888137d4
ricardo <ricardo@nasatto.com>
2024-06-05 13:27:24 -0300

    fix install_myenv

--- install_myenv.sh
+++ install_myenv.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 RELEASE=$(lsb_release -c | tail -n1 | cut -d":" -f2 | xargs)
-sed "s/$RELEASE/testing" /etc/apt/sources.list | tee /etc/apt/sources.list
+sed "s/$RELEASE/testing" /etc/apt/sources.list | sudo tee /etc/apt/sources.list
 
-apt update
-apt install -y i3 i3blocks dunst curl wget alacritty ranger git gnupg xbanish feh picom xinit stow htop neovim tmux btop batcat ripgrep fish zoxide zathura
+sudo apt update
+sudo apt install -y i3 i3blocks dunst curl wget alacritty ranger git gnupg xbanish feh picom xinit stow htop neovim tmux btop batcat ripgrep fish zoxide zathura
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
@@ -13,8 +13,9 @@ wget -qO- https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBr
 fc-cache -f
 
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
-apt update
-apt install brave-browser
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" \ 
+    | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
 
 stow --target=$HOME --dir=$HOME/dotfiles */
