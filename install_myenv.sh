84393a7ef2669a561d4bb0985babf46694a90fb3
ricardo <ricardo@nasatto.com>
2024-06-05 17:01:54 -0300

    more fix install_myenv

--- install_myenv.sh
+++ install_myenv.sh
@@ -8,14 +8,24 @@ deb http://debian.c3sl.ufpr.br/debian/ bookworm-backports main contrib non-free 
 deb https://deb.debian.org/debian unstable main contrib non-free-firmware
 EOF
 
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt dist-upgrade -y
-sudo apt install -y i3 i3blocks eza fontconfig dunst nodejs npm python3-pip python3-venv gcc zip luarocks curl jq wget alacritty ranger git gnupg pcscd vnstat acpi acpid xbanish feh picom xinit stow htop neovim tmux btop bat ripgrep fish zoxide zathura
+sudo apt install -y i3 i3blocks eza fontconfig autotiling dunst flameshot nodejs npm python3-pip python3-venv gcc zip luarocks curl jq wget alacritty ranger git gnupg pcscd vnstat acpi acpid xbanish feh picom xinit stow htop neovim tmux btop bat ripgrep fish zoxide zathura
 
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+sudo tee /etc/apt/sources.list.d/librewolf.sources <<EOF >/dev/null
+Types: deb
+URIs: https://deb.librewolf.net
+Suites: bookworm
+Components: main
+Architectures: amd64
+Signed-By: /usr/share/keyrings/librewolf.gpg
+EOF
+
 sudo apt update
-sudo apt install -y brave-browser
+sudo apt install -y brave-browser librewolf
 sudo apt autoremove --purge -y
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
