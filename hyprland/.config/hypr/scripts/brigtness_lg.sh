98f5066f6786e10ed9f0ef72eb01fc5263883200
ricardo <ricardo@nasatto.com>
2024-08-05 15:52:34 -0300

    daily changes

--- hyprland/.config/hypr/scripts/brigtness_lg.sh
+++ hyprland/.config/hypr/scripts/brigtness_lg.sh
@@ -2,13 +2,16 @@
 
 current=$(ddcutil --display=1 getvcp 0x10 | cut -d',' -f1 | awk '{print $9}')
 
-if [ "$1" == '+' ]; then
+case $1 in
++)
     current=$((current + $2))
-else
-    if [ "$1" == '-' ]; then
-        current=$((current - $2))
-    fi
-fi
-
-ddcutil --display=1 setvcp 0x10 $current
-echo $current
+    ddcutil --display=1 setvcp 0x10 $current
+    ;;
+-)
+    current=$((current - $2))
+    ddcutil --display=1 setvcp 0x10 $current
+    ;;
+*)
+    echo $current
+    ;;
+esac
