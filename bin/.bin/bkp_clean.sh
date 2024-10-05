445ae61116410dc191dcf527e852614bd05be7cb
ricardo <ricardo@nasatto.com>
2024-10-05 16:01:39 -0300

    restic backup clean script

--- bin/.bin/bkp_clean.sh
+++ bin/.bin/bkp_clean.sh
@@ -2,4 +2,4 @@
 
 /usr/bin/bash ~/.bin/restic-av.sh unlock
 /usr/bin/bash ~/.bin/restic-av.sh forget \
-    --keep-daily=7 --keep-weekly=4 --keep-monthly=12 --prune $@
+    --keep-daily=30 --keep-monthly=12 --prune $@
