7d32876abe284c809668d19c51614cad3af343c8
ricardo <ricardo@nasatto.com>
2024-10-05 16:02:30 -0300

    restic backup clean script

--- bin/.bin/bkp.sh
+++ bin/.bin/bkp.sh
@@ -4,8 +4,6 @@ DIR=/home/ricardo
 /usr/bin/bash $DIR/.bin/restic-av.sh unlock
 /usr/bin/bash $DIR/.bin/restic-av.sh backup --one-file-system \
     --exclude="*/node_modules/*" --exclude-caches=true $DIR/Documents
-# /usr/bin/bash $DIR/.bin/restic-av.sh forget \
-# --keep-daily=7 --keep-weekly=4 --keep-monthly=12 --prune
 
 /usr/bin/pass git pull
 /usr/bin/pass git push
