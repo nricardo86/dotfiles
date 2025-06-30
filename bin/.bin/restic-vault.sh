471916bf62161e03966b06a5d397b92a2c4a7126
ricardo <ricardo@nasatto.com>
2025-06-30 14:39:51 -0300

    weekly changes

--- bin/.bin/restic-vault.sh
+++ bin/.bin/restic-vault.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export RESTIC_REPOSITORY='sftp:vault:/data/asdfg98'
+export RESTIC_REPOSITORY='sftp:root@10.0.4.57:/data/asdfg98'
 export RESTIC_PASSWORD_COMMAND='pass restic/asdfg98'
 #export RESTIC_PASSWORD=''
 export RESTIC_READ_CONCURRENCY=5
