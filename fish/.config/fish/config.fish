7fda8e4d90fdcb7ab88c908f0feae00db7de4971
ricardo <ricardo@nasatto.com>
2026-01-16 16:04:41 -0300

    fish config add abbr git add . && git commit && git push

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -48,6 +48,7 @@ abbr img "chafa"
 abbr b "batcat"
 abbr 2fa "wl-copy -o (oathtool -b --totp=sha1 (pass 2fa ))"
 abbr pff "pass ff"
+abbr gp "git add . && git commit && git push"
 
 function gbr --description "Git browse commits"
     set -l log_line_to_hash "echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
