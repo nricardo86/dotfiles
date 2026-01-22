5ab1684006ffad5a28ca572e471bf8690c14e71a
ricardo <ricardo@nasatto.com>
2026-01-22 12:39:55 -0300

    fish gbr function change copy to wl-copy

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -53,7 +53,7 @@ abbr dsf "diff-so-fancy"
 function gbr --description "Git browse commits"
     set -l log_line_to_hash "echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
     set -l view_commit "$log_line_to_hash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy | less -R'"
-    set -l copy_commit_hash "$log_line_to_hash | xclip"
+    set -l copy_commit_hash "$log_line_to_hash | wl-copy"
     set -l git_checkout "$log_line_to_hash | xargs -I % sh -c 'git checkout %'"
     set -l open_cmd "open"
 
