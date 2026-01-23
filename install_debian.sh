20404fd40544abc4b61e2ba7147dcb7bf991f3cd
ricardo <ricardo@nasatto.com>
2026-01-23 10:31:40 -0300

    update install_debian scripts

--- install_debian.sh
+++ install_debian.sh
@@ -38,6 +38,33 @@ EOF
 sudo apt update
 sudo apt upgrade -y
 # install dependencies
+sudo apt install -fy build-essential cmake cmake-extras curl findutils \
+    gawk gettext gir1.2-graphene-1.0 git glslang-tools gobject-introspection \
+    golang hwdata jq libavcodec-dev libavformat-dev libavutil-dev \
+    libcairo2-dev libdeflate-dev libdisplay-info-dev libdrm-dev libegl-dev \
+    libegl1-mesa-dev libgbm-dev libgdk-pixbuf-2.0-dev libgdk-pixbuf2.0-bin \
+    libgirepository1.0-dev libgl1-mesa-dev libgraphene-1.0-0 \
+    libgraphene-1.0-dev libgtk-3-dev libgulkan-0.15-0t64 libgulkan-dev \
+    libinih-dev libiniparser-dev libinput-dev libjbig-dev libjpeg-dev \
+    libjpeg62-turbo-dev liblerc-dev libliftoff-dev liblzma-dev libnotify-bin \
+    libpam0g-dev libpango1.0-dev libpipewire-0.3-dev libqt6svg6 \
+    libsdbus-c++-dev libseat-dev libstartup-notification0-dev \
+    libswresample-dev libsystemd-dev libtiff-dev libtiffxx6 \
+    libtomlplusplus-dev libudev-dev libvkfft-dev libvulkan-dev \
+    libvulkan-volk-dev libwayland-dev libwebp-dev libxcb-composite0-dev \
+    libxcb-cursor-dev libxcb-dri3-dev libxcb-ewmh-dev libxcb-icccm4-dev \
+    libxcb-present-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-util-dev \
+    libxcb-xinerama0-dev libxcb-xinput-dev libxcb-xkb-dev libxkbcommon-dev \
+    libxkbcommon-x11-dev libxkbregistry-dev libxml2-dev libxxhash-dev \
+    meson ninja-build openssl psmisc python3-mako python3-markdown \
+    python3-markupsafe python3-pyquery python3-yaml qt6-base-dev scdoc seatd \
+    spirv-tools unzip vulkan-utility-libraries-dev vulkan-validationlayers \
+    wayland-protocols xdg-desktop-portal xwayland
+sudo apt install -fy bc binutils libc6 libcairo2-dev libdisplay-info3 libdrm2 \
+    libjpeg-dev libjxl-dev libmagic-dev libmuparser-dev libpixman-1-dev \
+    libpugixml-dev libre2-dev librsvg2-dev libspng-dev libtomlplusplus-dev \
+    libwebp-dev libzip-dev libpam0g-dev libxcursor-dev qt6-declarative-dev \
+    qt6-base-private-dev qt6-wayland-dev qt6-wayland-private-dev
 sudo apt install -fy libdw1t64 usb.ids i2c-tools libarchive13t64 libmalcontent-0-0 \
     libostree-1-1 libinotifytools0 libbluetooth3 libmm-glib0 libndp0 libnm0 \
     libteamdctl0 python3-pyqt5 python3-dbus.mainloop.pyqt5 python3-dbus \
@@ -51,7 +78,8 @@ sudo apt install -fy hypr* xdg-dbus-proxy xdg-desktop-portal-hyprland fzf bluez 
     zig wlsunset inotify-tools ghostty \
     lazygit network-manager playerctl yazi uv waybar wofi pavucontrol-qt \
     librewolf libreoffice pulseaudio* brightnessctl ddcutil flatpak rfkill \
-    wireguard wireguard-tools tlp tlp-rdw tlp-pd upower grim swappy
+    wireguard wireguard-tools tlp tlp-rdw tlp-pd upower grim swappy qt5ct \
+    qt6ct yad xdg-utils mpv pamixer nvtop nwg-look nwg-displays
 
 sudo apt modernize-sources
 
