658601d9f45cfdc8938dbe3b9535fedeb2614002
ricardo <ricardo@nasatto.com>
2026-02-03 17:04:16 -0300

    update zfs2tape script

--- bin/.bin/server/zfs2tape.sh
+++ bin/.bin/server/zfs2tape.sh
@@ -16,21 +16,11 @@ function sizeRemain {
 }
 
 function send2tape {
-	sizeRemain
-	sizeSnap $@
-
-	if [[ ! "${SIZE_REMAIN_MB}" -ge "${SNAP_SIZE_MB}" ]]; then
-		echo "Insuficient remain space on tape"
-		zfs destroy ${NOW_SNAPTSHOT} &>/dev/null
-	else
-		zfs send -wc $@ | dd status=progress of=${TAPE} bs=${BS}
-	fi
-
-	echo -e "\n"
+	zfs send -wc ${@} | dd status=progress of=${TAPE} bs=${BS}
 }
 
 function findEOM {
-	echo "Finding EOM.."
+	echo "Finding EOM..."
 	count=3
 	while true; do
 		mt -f ${TAPE} eom
@@ -38,40 +28,62 @@ function findEOM {
 			break
 		fi
 		if [[ "${count}" -eq "0" ]]; then
-			echo "EOM not Found"
+			echo "EOM not Found!"
 			exit "$?"
 		fi
 		((count--))
 		mt -f ${TAPE} rewind
 	done
-	echo -e "EOM Found!\n"
+	echo "EOM Found!"
+	files_ontape=$(mt -f ${TAPE} status | grep 'file number' | awk '{print $4}')
 }
 
 function tapeRewind {
+	echo "Rewinding Tape!"
 	mt -f $TAPE rewind
 }
 
 function tapeEject {
+	echo "Rewinding and Ejecting Tape!"
 	mt -f $TAPE eject
 }
 
 function firstSnapshot {
 	echo "First Snapshot"
 	SNAP_INIT=$(zfs list -t snapshot $1 -H | grep $2 | head -n1 | awk '{print $1}')
-	echo "Sending from ${SNAP_INIT}"
-	send2tape "-R ${SNAP_INIT}"
+
+	sizeRemain
+	sizeSnap "-I ${SNAP_INIT}"
+
+	if [[ ! "${SIZE_REMAIN_MB}" -ge "${SNAP_SIZE_MB}" ]]; then
+		echo "Insuficient remain space on tape"
+	else
+		echo "Sending from ${SNAP_INIT}"
+		send2tape "-R ${SNAP_INIT}"
+	fi
+
 	files_ontape=1
 }
 
 function snapshot {
 	LAST_SNAPSHOT=$(zfs list -t snapshot -H -o name $1 | grep $2 | tail -1)
-	zfs snapshot $@-$(date --utc +%Y%m%d-%H%M)
+	zfs snapshot "${1}@${2}-$(date --utc +%Y%m%d-%H%M)"
 	NOW_SNAPSHOT=$(zfs list -t snapshot -H -o name $1 | grep $2 | tail -1)
-	echo "Incremental Snapshot from ${LAST_SNAPSHOT} to ${NOW_SNAPSHOT}"
-	send2tape "-I ${LAST_SNAPSHOT} ${NOW_SNAPSHOT}"
+
+	sizeRemain
+	sizeSnap "-I ${LAST_SNAPSHOT} ${NOW_SNAPSHOT}"
+
+	if [[ ! "${SIZE_REMAIN_MB}" -ge "${SNAP_SIZE_MB}" ]]; then
+		echo "Insuficient remain space on tape"
+		zfs destroy ${NOW_SNAPTSHOT} &>/dev/null
+	else
+		echo "Incremental Snapshot from ${LAST_SNAPSHOT} to ${NOW_SNAPSHOT}"
+		send2tape "-I ${LAST_SNAPSHOT} ${NOW_SNAPSHOT}"
+	fi
 }
 
 function recursiveSnapshots {
+	local count=0
 	local snapshots=()
 	for i in $(zfs list -t snapshot $1 -H -o name); do
 		if [[ "$i" == *"$2"* ]]; then
@@ -83,36 +95,45 @@ function recursiveSnapshots {
 	while [ "${count}" -gt "1" ]; do
 		SNAP_FROM=${snapshots[((${#snapshots[@]} - ${count}))]}
 		SNAP_TO=${snapshots[((${#snapshots[@]} - ((${count} - 1))))]}
-		echo "Sending incremental snapshot from ${SNAP_FROM} to ${SNAP_TO}"
-		send2tape "-I ${SNAP_FROM} ${SNAP_TO}"
+
+		sizeRemain
+		sizeSnap "-I ${SNAP_FROM} ${SNAP_TO}"
+
+		if [[ ! "${SIZE_REMAIN_MB}" -ge "${SNAP_SIZE_MB}" ]]; then
+			echo "Insuficient remain space on tape"
+			exit 1
+		else
+			echo "Sending incremental snapshot from ${SNAP_FROM} to ${SNAP_TO}"
+			send2tape "-I ${SNAP_FROM} ${SNAP_TO}"
+		fi
 		((count--))
 	done
 }
 
 function main {
-	DS=$1
-	prefix="${2:-tapebkp}"
+	local DS=${1}
+	local prefix="${2:-tapebkp}"
 
-	if [[ -z "$DS" ]]; then
+	if [[ -z "${DS}" ]]; then
 		echo "need dataset to backup"
 		exit 1
 	fi
 
+	echo "Begin of backup - $(date --utc +%Y/%m/%d-%H:%M)"
+	echo "----------------------------------"
+
 	TAPE_SERIAL=$(sudo sg_read_attr ${TAPE} -f 0x401 | awk '{print $4}')
-	if [[ -z $TAPE_SERIAL ]]; then
+	if [[ -z ${TAPE_SERIAL} ]]; then
 		echo "fail to identify tape"
 		exit 2
+	else	
+		echo "Tape Serial: ${TAPE_SERIAL}"
 	fi
 
-	echo -e "Begin of backup - $(date --utc +%Y/%m/%d-%H:%M)\n"
-	echo "Tape Serial: ${TAPE_SERIAL}"
-
+	tapeRewind
 	findEOM
 
-	files_ontape=$(mt -f ${TAPE} status | grep 'file number' | awk '{print $4}')
-
-	if [ "$files_ontape" -eq 0 ]; then
-		echo "Rewinding Tape"
+	if [ "${files_ontape}" -eq 0 ]; then
 		tapeRewind
 		firstSnapshot "${DS}" "${prefix}"
 	fi
@@ -120,9 +141,9 @@ function main {
 	recursiveSnapshots "${DS}" "${prefix}"
 	snapshot "${DS}" "${prefix}"
 
-	echo -e "Rewinding and Unloading Tape!\n"
-	tapeEject
+	#tapeEject
 
+	echo "--------------------------------"
 	echo "End of backup - $(date --utc +%Y/%m/%d-%H:%M)"
 }
 
