c5abe722ec4e34bd39235f6ec858445c26810b22
ricardo <ricardo@nasatto.com>
2026-02-16 09:53:50 -0300

    impruve install script

--- install_debian.sh
+++ install_debian.sh
@@ -29,8 +29,8 @@ Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 EOF
 
 sudo apt update
-sudo apt dist-upgrade -y
-sudo apt install -y bc eza rsync fontconfig restic pass npm nodejs \
+sudo apt dist-upgrade -qy
+sudo apt install -qy bc eza rsync fontconfig restic pass npm nodejs \
     python3 python3-pip python3-venv gcc zip luarocks curl jq wget git gnupg \
     scdaemon vnstat acpi acpid stow doas htop neovim tmux btop bat ripgrep \
     fish zsh zoxide bash-completion smartmontools extrepo
@@ -57,7 +57,7 @@ sudo apt update
 sudo apt upgrade -y
 
 # install dependencies
-sudo apt install -fy build-essential cmake cmake-extras curl findutils \
+sudo apt install -qfy build-essential cmake cmake-extras curl findutils \
     gawk gettext gir1.2-graphene-1.0 git glslang-tools gobject-introspection \
     golang hwdata jq libavcodec-dev libavformat-dev libavutil-dev \
     libcairo2-dev libdeflate-dev libdisplay-info-dev libdrm-dev libegl-dev \
@@ -68,25 +68,25 @@ sudo apt install -fy build-essential cmake cmake-extras curl findutils \
     libjpeg62-turbo-dev liblerc-dev libliftoff-dev liblzma-dev libnotify-bin \
     libpam0g-dev libpango1.0-dev libpipewire-0.3-dev libqt6svg6 \
     libsdbus-c++-dev libseat-dev libstartup-notification0-dev
-sudo apt install -fy libswresample-dev libsystemd-dev libtiff-dev libtiffxx6 \
+sudo apt install -qfy libswresample-dev libsystemd-dev libtiff-dev libtiffxx6 \
     libtomlplusplus-dev libudev-dev libvkfft-dev libvulkan-dev \
     libvulkan-volk-dev libwayland-dev libwebp-dev libxcb-composite0-dev \
     libxcb-cursor-dev libxcb-dri3-dev libxcb-ewmh-dev libxcb-icccm4-dev \
     libxcb-present-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-util-dev \
     libxcb-xinerama0-dev libxcb-xinput-dev libxcb-xkb-dev libxkbcommon-dev \
     libxkbcommon-x11-dev libxkbregistry-dev libxml2-dev libxxhash-dev
-sudo apt install -fy meson ninja-build openssl psmisc python3-mako \
+sudo apt install -qfy meson ninja-build openssl psmisc python3-mako \
     python3-markdown python3-markupsafe python3-pyquery python3-yaml \
     qt6-base-dev scdoc seatd spirv-tools unzip vulkan-utility-libraries-dev \
     vulkan-validationlayers wayland-protocols xdg-desktop-portal xwayland \
     liblua5.2-0 libmujs3 libsixel1 libva-wayland2 libxpresent1 yt-dlp \
     gir1.2-gtklayershell-0.1 python3-i3ipc xcur2png
-sudo apt install -fy bc binutils libc6 libcairo2-dev libdisplay-info3 libdrm2 \
+sudo apt install -qfy bc binutils libc6 libcairo2-dev libdisplay-info3 libdrm2 \
     libjpeg-dev libjxl-dev libmagic-dev libmuparser-dev libpixman-1-dev \
     libpugixml-dev libre2-dev librsvg2-dev libspng-dev libtomlplusplus-dev \
     libwebp-dev libzip-dev libpam0g-dev libxcursor-dev qt6-declarative-dev \
     qt6-base-private-dev qt6-wayland-dev qt6-wayland-private-dev
-sudo apt install -fy libdw1t64 usb.ids i2c-tools libarchive13t64 libmalcontent-0-0 \
+sudo apt install -qfy libdw1t64 usb.ids i2c-tools libarchive13t64 libmalcontent-0-0 \
     libostree-1-1 libinotifytools0 libbluetooth3 libmm-glib0 libndp0 libnm0 \
     libteamdctl0 python3-pyqt5 python3-dbus.mainloop.pyqt5 python3-dbus \
     libbluetooth3 liblirc-client0t64 avahi-daemon hdparm iw \
@@ -95,7 +95,7 @@ sudo apt install -fy libdw1t64 usb.ids i2c-tools libarchive13t64 libmalcontent-0
     libqt5network5t64 libqt5printsupport5t64 libqt5test5t64 libqt5widgets5t64 \
     libmm-glib0 libqt5xml5t64 libxcb-xinerama0 libqt5help5
 # install apps
-sudo apt install -fy xdg-dbus-proxy xdg-desktop-portal-hyprland fzf bluez \
+sudo apt install -qfy xdg-dbus-proxy xdg-desktop-portal-hyprland fzf bluez \
     hyprland hyprcursor-util hypridle hyprland-protocols hyprland-qtutils \
     hyprlock hyprpaper hyprpicker hyprwayland-scanner hyprland-backgrounds \
     zig wlsunset inotify-tools ghostty lazygit network-manager playerctl yazi \
@@ -107,6 +107,9 @@ sudo apt install -fy xdg-dbus-proxy xdg-desktop-portal-hyprland fzf bluez \
     xdg-utils mpv pamixer nvtop nwg-look nwg-displays adwaita-icon-theme \
     adwaita-qt adwaita-qt6 gnome-themes-extra cups breeze-icon-theme \
     breeze-cursor-theme breeze-gtk-theme
+
+#cleaning up install
+sudo apt autoremove --purge -qy
 
 #set fish shell default for current user
 sudo chsh -s $(which fish) $USER
