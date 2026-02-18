47e0d5bf95cf4ae2c452e7c2a42899e21b161838
ricardo <ricardo@nasatto.com>
2026-02-18 17:29:30 -0300

    config tofi and idrive script

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -6,7 +6,7 @@ return { -- Autocompletion
 		-- Snippet Engine
 		{
 			"L3MON4D3/LuaSnip",
-			version = "2.*",
+			version = "v2.*",
 			build = (function()
 				-- Build Step is needed for regex support in snippets.
 				-- This step is not supported in many windows environments.
@@ -20,12 +20,12 @@ return { -- Autocompletion
 				-- `friendly-snippets` contains a variety of premade snippets.
 				--    See the README about individual language/framework/plugin snippets:
 				--    https://github.com/rafamadriz/friendly-snippets
-				-- {
-				--   'rafamadriz/friendly-snippets',
-				--   config = function()
-				--     require('luasnip.loaders.from_vscode').lazy_load()
-				--   end,
-				-- },
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
 			},
 			opts = {},
 		},
