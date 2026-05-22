edb9b7f8b201688611d94e4cb88f3e49576bf9d1
ricardo <ricardo@nasatto.com>
2026-05-22 11:20:48 -0300

    bashrc fixes and nvim update plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -34,6 +34,18 @@ return {
 	},
 	{ "junegunn/fzf" },
 	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- or if using mini.icons/mini.nvim
+		-- dependencies = { "nvim-mini/mini.icons" },
+		---@module "fzf-lua"
+		---@type fzf-lua.Config|{}
+		---@diagnostic disable: missing-fields
+		opts = {},
+		---@diagnostic enable: missing-fields
+	},
+	{
 		"stevearc/oil.nvim",
 		opts = {
 			columns = { "icon" },
