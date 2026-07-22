07284e4c76458d594c56bbda45e1adb94dc9824f
ricardo <ricardo@nasatto.com>
2026-07-22 13:45:22 -0300

    remove server scripts from repo and updating bkp script

--- bin/.bin/server/nhs/start.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-# Define the command to run in the background
-command = ['python3', '/home/admin/nhs/nhs.py']
-# Run the command in the background
-subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
